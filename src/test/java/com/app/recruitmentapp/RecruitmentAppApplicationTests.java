0954379e07f75f7da74a641e497d5a18a9072e5e
Raed.Ghodhbane <ghodhbane.raed@gmail.com>
2025-06-10 19:27:14 +0200

    Update project

---
+++ src/test/java/com/app/recruitmentapp/RecruitmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.recruitmentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
