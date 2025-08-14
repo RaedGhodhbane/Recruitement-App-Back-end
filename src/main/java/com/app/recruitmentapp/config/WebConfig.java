614831e81d8376a646f97fc1389ad5a0f4c620f7
Raed.Ghodhbane <ghodhbane.raed@gmail.com>
2025-08-14 13:48:31 +0100

    Edit in WebConfig file

--- src/main/java/com/app/recruitmentapp/config/WebConfig.java
+++ src/main/java/com/app/recruitmentapp/config/WebConfig.java
@@ -14,7 +14,9 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("*");
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
