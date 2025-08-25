ac6cd292623ecb8490f8b00f1211267fb0bf2304
Raed.Ghodhbane <ghodhbane.raed@gmail.com>
2025-08-25 10:30:33 +0100

    Creation of the Entity "Favourite"

---
+++ src/main/java/com/app/recruitmentapp/entities/Favourite.java
@@ -0,0 +1,47 @@
+package com.app.recruitmentapp.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "saved_jobs")
+@NoArgsConstructor @AllArgsConstructor
+public class Favourite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "offer_id", nullable = false)
+    private Offer offer;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Offer getOffer() {
+        return offer;
+    }
+
+    public void setOffer(Offer offer) {
+        this.offer = offer;
+    }
+}
+
