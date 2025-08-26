bc004edef900ca835867fef3d9d916dfc1264f49
Raed.Ghodhbane <ghodhbane.raed@gmail.com>
2025-08-26 16:55:54 +0100

    Edit in the Main

--- src/main/java/com/app/recruitmentapp/RecruitmentAppApplication.java
+++ src/main/java/com/app/recruitmentapp/RecruitmentAppApplication.java
@@ -2,8 +2,9 @@ package com.app.recruitmentapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.app.recruitmentapp")
 public class RecruitmentAppApplication {
 
     public static void main(String[] args) {
