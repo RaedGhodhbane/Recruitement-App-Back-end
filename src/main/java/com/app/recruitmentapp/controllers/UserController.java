101789a7ce57990a25d7564dd8d752f9192a4c03
Raed.Ghodhbane <ghodhbane.raed@gmail.com>
2025-09-09 10:41:39 +0100

    Edit "UserController" in RequestMapping's path

--- src/main/java/com/app/recruitmentapp/controllers/UserController.java
+++ src/main/java/com/app/recruitmentapp/controllers/UserController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/admin/user")
 public class UserController {
     @Autowired
     private UserService userService;
